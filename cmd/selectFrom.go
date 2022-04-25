cd194810f9d792bce02d0b1be0111b4668e57066
Thiago Bastos <thiagosbastos@live.com>
2022-04-25 22:20:59 +0200

    Add knapsack problem optimization.

--- cmd/selectFrom.go
+++ cmd/selectFrom.go
@@ -6,15 +6,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/spf13/cobra"
 )
 
 type Backlog struct {
-	TargetStoryPoints int      `json:"targetStoryPoints"`
-	Tasks             []string `json:"tasks"`
-	StoryPoints       []int    `json:"storyPoints"`
-	Priorities        []int    `json:"priorities"`
+	TargetStoryPoints int
+	Tasks             []string
+	StoryPoints       []int
+	Priorities        []int
+}
+
+type Sprint struct {
+	TotalStoryPoints int
+	Tasks            []string
+	StoryPoints      []int
+	Priorities       []int
 }
 
 var selectFromCmd = &cobra.Command{
@@ -53,11 +61,89 @@ var selectFromCmd = &cobra.Command{
 		}
 
 		// TODO: 4. Analyze
+		sprint := findOptimalSolution(backlog)
 
 		// TODO: 5. Give feedback upon finishing analysis and having results
+		fmt.Printf(
+			"TotalStoryPoints: %d\nTasks: %s\nStoryPoints: %d\nPriorities: %d\n",
+			sprint.TotalStoryPoints, sprint.Tasks, sprint.StoryPoints, sprint.Priorities,
+		)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(selectFromCmd)
+}
+
+// Solves the knapsack problem using dynamic programming
+func findOptimalSolution(backlog Backlog) Sprint {
+	numberOfTasks := len(backlog.Tasks)
+	maxStoryPoints := backlog.TargetStoryPoints
+
+	// 1. construct a values and a areKept tables
+	values := make([][]int, numberOfTasks+1) // row with the 'items' to pick
+	areKept := make([][]bool, numberOfTasks+1)
+	for i := 0; i < numberOfTasks+1; i++ {
+		values[i] = make([]int, maxStoryPoints+1) // column with the 'weights'
+		areKept[i] = make([]bool, maxStoryPoints+1)
+	}
+
+	// 2a. Base case: no items in the 'knapsack'
+	for j := 0; j < maxStoryPoints+1; j++ {
+		values[0][j] = 0
+		areKept[0][j] = false
+	}
+	// 2b. Base case: knapsack with no weight
+	for i := 0; i < numberOfTasks+1; i++ {
+		values[i][0] = 0
+		areKept[i][0] = false
+	}
+
+	// 3. Fill tables with the values
+	for i := 1; i <= numberOfTasks; i++ {
+		for j := 1; j <= maxStoryPoints; j++ {
+
+			taskFits := (backlog.StoryPoints[i-1] <= j)
+			if !taskFits {
+				continue
+			}
+
+			currMaxValue := backlog.Priorities[i-1] + values[i-1][j-backlog.StoryPoints[i-1]]
+			prevMaxValue := values[i-1][j]
+
+			if currMaxValue > prevMaxValue {
+				values[i][j] = currMaxValue
+				areKept[i][j] = true
+			} else {
+				values[i][j] = prevMaxValue
+				areKept[i][j] = false
+			}
+		}
+	}
+
+	// 4. Retrieve the indices of the values that give the optimal solution
+	n := numberOfTasks
+	c := maxStoryPoints
+	var indices []int
+	for n > 0 {
+		if areKept[n][c] {
+			indices = append(indices, n-1)
+			c -= backlog.StoryPoints[n-1]
+		}
+		n--
+	}
+
+	// 5. Fill the solution data structure
+	var sprint Sprint
+	sprint.TotalStoryPoints = 0
+
+	sort.Ints(indices)
+	for i, idx := range indices {
+		sprint.Tasks = append(sprint.Tasks, backlog.Tasks[idx])
+		sprint.StoryPoints = append(sprint.StoryPoints, backlog.StoryPoints[idx])
+		sprint.Priorities = append(sprint.Priorities, backlog.Priorities[idx])
+		sprint.TotalStoryPoints += sprint.StoryPoints[i]
+	}
+
+	return sprint
 }
